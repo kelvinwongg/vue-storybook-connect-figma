57fb1274b21b379b4692bba9b7f2ca079b05fb64
kelvin <kelvin@macbook>
2022-05-16 18:27:23 +0800

    Config storybook & Embed Figma into component stories file

--- src/components/TextBanner.stories.js
+++ src/components/TextBanner.stories.js
@@ -3,6 +3,12 @@ import TextBanner from './TextBanner';
 export default {
 	title: 'Example/TextBanner',
 	component: TextBanner,
+	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/s2DRcsFSL3S1Za936rYBWJ/Figma-Connect-1?node-id=0%3A1'
+		}
+	}
 };
 
 const Template = (args, { argTypes }) => ({
